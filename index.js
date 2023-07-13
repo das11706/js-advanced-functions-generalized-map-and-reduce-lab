24c2df5135a9d4544097c59f6fbf6dcafb766e78
Daniel Suarez <suarez.daniel101@gmail.com>
2023-07-13 18:00:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+
+function map(array, func) {
+  const newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const val = array[i];
+    newArray.push(func(val));
+  }
+  return newArray;
+}
+
+
+function reduce(sourceArray, func, startingValue) {
+  let total;
+  if(startingValue) {
+    total = startingValue;
+    for (let i = 0; i < sourceArray.length; i++) {
+      total = func(sourceArray[i], total);
+    }
+    return total;
+  } else {
+    total = sourceArray[0];
+    for (let i = 1; i < sourceArray.length; i++) {
+      total = func(sourceArray[i], total);
+    }
+    return total;
+  }
+}
